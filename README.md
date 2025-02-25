05fcf49a30315d266405e6a43561102f083a3bda
David Vaquero <pepesan@gmail.com>
2025-02-25 16:34:19 +0100

    primeros pasos para la release 0.1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Proyecto de ejemplo de Git para Indra
+Version de 2025
--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Version de codigo 0.1
 // codigo de manejko de load de la pagina
 // se ejecuta cuando la pagina se carga en Javascrip standart
 document.addEventListener('DOMContentLoaded', function() {
